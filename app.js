a98917970f90bb40aa721989aa86e14398f87096
varooon <varuns@Varuns-MacBook-Pro.local>
2016-11-10 18:43:51 -0800

    tictactoe

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ $(document).ready(function(){
 
   // turn variable will determine X or O & begin with X
   var turn = 1;
-  var x = "<div id='x'><h1>X</h1></div>";
-  var o = "<div id='o'><h1>O</h1></div>";
+  var x = "<h1>X</h1>";
+  var o = "<h1>O</h1>";
+  var userValues = [];
 
 
   // As each custom-box div is clicked...
@@ -13,18 +14,36 @@ $(document).ready(function(){
       // console.log("empty"); <-- Test to see if console displays "empty" on empty cells
       if(turn===1){
         $(this).html(x);
-        console.log($("#one"));
         //update turn variable to ensure next click will be 'O'
         turn = 0;
       } else if (turn===0){
         $(this).html(o);
         turn = 1;
       }
+
+
+      // Storing X & O values in an array
+      userValues = $(".custom-box > div").get();
+      console.log(userValues[0]);
+      // console.log(userValues[1]);
+      // console.log(userValues[2]);
+      // console.log(userValues[3]);
+      // console.log(userValues[4]);
+      // console.log(userValues[5]);
+      // console.log(userValues[6]);
+      // console.log(userValues[7]);
+      // console.log(userValues[8]);
+
     } else{
-      //display message since the spot is already taken
-      console.log("Try again. This box is taken.");
+      //display alert since the spot is already taken
+      alert("Try again. This box is taken.");
     }
   });
+
+
+  if (userValues[0]==="X" && userValues[1]==="X" && userValues[2]==="X" ){
+    alert("winner!");
+  }
 
 
 
